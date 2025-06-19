c4de7bc3c908291f3514931c7554ffc7ffcc8550
Henrique <suportecrm@plurismidia.com.br>
2025-06-19 12:02:00 -0300

    chore: atualiza dependências e instala po-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+} from '@po-ui/ng-components';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ttt-po';
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item');
+  }
 }
